2e499439161aaa960da78c76bb2604f6254d55b2
zenyinnaya-star <>
2026-03-10 19:59:08 +0000

    Update and Delete

---
+++ ddl_and_dml_in_sql/update_and_delete/query.sql
@@ -0,0 +1,11 @@
+-- Write your solution here
+Update medications
+    set price=10
+    where price<10;
+
+
+
+-- Please do not modify the code below!
+SELECT * FROM medications
+ORDER BY id;
+-- Please do not modify the code above!
